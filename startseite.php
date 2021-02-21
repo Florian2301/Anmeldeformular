792bee5a8f8d5748c70c9bcff1967e4fefed3be8
Flokrates2301 <67049493+Flokrates2301@users.noreply.github.com>
2021-02-21 17:56:27 +0100

    upload

---
+++ startseite.php
@@ -0,0 +1,22 @@
+<?php
+include "./components/htmlelemente.php";
+htmlanfang("Willkommen zur WTC 2021");
+include "./components/navigation.php";
+?>
+
+<h1 class='container'>Willkommen zur webconia Technology Conference 2021</h1>
+
+<div class='container' style='margin-top: 3rem'>
+
+    <h5>Nach über einem Jahr Pause startet diesen Sommer wieder die webconia Technology Conference.</h5> 
+    
+    <p style='margin-top: 2rem'>Mit dabei sind wieder interne sowie externe Redner, die über Themen wie die neuesten Technologien und Innovationen, <br>
+    Trends und Produken berichten werden. Das ganze findet wie gewohnt im Herzen von Hamburg am Gänsemarkt statt.</p>
+    
+    <p>Zur Anmeldung zur WTC2021 geht es <a href='formular.php' class='teilnehmer'>hier</a></p>
+
+</div>
+
+<?php
+htmlende();
+?>
